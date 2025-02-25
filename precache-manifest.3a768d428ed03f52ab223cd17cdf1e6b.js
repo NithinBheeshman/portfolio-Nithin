ab687388d5d22da34e70c607caa1f027f66ada35
Nithin <apple@Rohiriim.local>
2025-02-24 23:55:45 -0800

    Updates

---
+++ precache-manifest.3a768d428ed03f52ab223cd17cdf1e6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10feec4dd98732b85bbc0cf17db32a2e",
+    "url": "/portfolio-Nithin/index.html"
+  },
+  {
+    "revision": "ae35dc41a6d4a0522515",
+    "url": "/portfolio-Nithin/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "567b79ded998ae30c4ad",
+    "url": "/portfolio-Nithin/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "ae35dc41a6d4a0522515",
+    "url": "/portfolio-Nithin/static/js/2.745d4e72.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio-Nithin/static/js/2.745d4e72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "567b79ded998ae30c4ad",
+    "url": "/portfolio-Nithin/static/js/main.a53c1f3a.chunk.js"
+  },
+  {
+    "revision": "5714557bd5755c2411e7",
+    "url": "/portfolio-Nithin/static/js/runtime-main.9478e9c0.js"
+  }
+]);
